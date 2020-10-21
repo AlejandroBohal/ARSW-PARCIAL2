7089e76b1891b83935140aab6923ed04e30d3282
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-10-21 14:59:16 -0500

    Mapa

--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -5,6 +5,7 @@ const app = (() =>{
         let table3 = $("#mainStats > tbody");
         let table0 = $("#cityinfo > tbody");
         let wd = weatherStats.data;
+        plotMarkers(wd.coord)
         table0.empty();
         table1.empty();
         table2.empty();
@@ -36,10 +37,38 @@ const app = (() =>{
         );
 
     }
+    const init = () =>{
+        initMap();
+    }
+    var initMap = () => {
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 2,
+            center: {lat: 35.717, lng: 139.731},
+        });
+    }
+    function plotMarkers(m) {
+        console.log(m)
+        markers = [];
+        bounds = new google.maps.LatLngBounds();
+        console.log(m.latitude, m.longitude);
+        var position = new google.maps.LatLng(m.lat, m.lon);
+        console.log(position);
+        markers.push(
+            new google.maps.Marker({
+                position: position,
+                map: map,
+                animation: google.maps.Animation.DROP
+            })
+        );
+        bounds.extend(position);
+        map.fitBounds(bounds);
+        map.setZoom(4);
+    }
     const getWeatherByCity = (city) =>{
         weatherservice.getWeatherByCity(city,mapWeatherToObject);
     }
     return{
-        getWeatherByCity:getWeatherByCity
+        getWeatherByCity:getWeatherByCity,
+        init:init
     }
 })();
